ff07626b7f03678aaef30d8311f8ce7f4ca05f6a
hiro88hyo <hiro88hyo@gmail.com>
2018-08-17 10:18:16 -0400

    Disabled fancybox fot flickr embed

--- themes/hipaper/source/js/script.js
+++ themes/hipaper/source/js/script.js
@@ -95,7 +95,7 @@
 
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      //$(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
 
     $(this).find('.fancybox').each(function(){
